309d36152ca13469e59471ba7f962f7600e1e8c0
liranmanzouri <liranman77@gmail.com>
2025-06-19 16:44:37 +0300

    Finished the Exercise

--- users/src/App.tsx
+++ users/src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import UserList from "./features/UserList";
+import User from "./features/User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserList>
+          <User name={'Liran'}></User>
+          <User name={'Ido'}></User>
+      </UserList>
     </div>
   );
 }
